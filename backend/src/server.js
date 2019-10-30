2125f844c3ea39727236a45eb3773d353a464692
Samuel Oliveira <Samuel Oliveira>
2019-10-30 00:07:23 -0300

    Finalizado parte do frontend WEB em reacJS

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ const routes = require('./routes');
 const mongoose = require('mongoose');
 const app = express();
 const cors = require('cors');
+const path = require('path');
 
 mongoose.connect('mongodb+srv://Samuel:samuel@myappomni-7yoss.mongodb.net/MyAppNode?retryWrites=true&w=majority',
 {
@@ -18,6 +19,7 @@ mongoose.connect('mongodb+srv://Samuel:samuel@myappomni-7yoss.mongodb.net/MyAppN
 
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')))
 app.use(routes);
 app.listen(3333);
 
